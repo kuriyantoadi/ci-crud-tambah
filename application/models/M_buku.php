1e035f10bcf9953ec2f319fd6df3a49721efcca0
kuriyantoadi <melatikuning212@gmail.com>
2019-12-01 16:00:07 +0700

    edit kerapian

--- application/models/M_buku.php
+++ application/models/M_buku.php
@@ -1,7 +1,6 @@
 <?php
-class M_buku extends CI_Model
-{
+class M_buku extends CI_Model{
  function tambah_data($data_buku){
-  $this->db->insert('tb_buku',$data_buku);
+   $this->db->insert('tb_buku',$data_buku);
  }
-}?>
+}
